0496f6b0c0c6359be76da5a9d456b9d7397dd0c6
shuvam_776 <shuvamsatapathi@gmail.com>
2025-10-27 23:06:13 +0530

    A bit responsiveness

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const startButton = document.getElementById("buttonstart");
   const scoreDiv = document.getElementById("scorediv");
   const body = document.getElementById("body");
-  scoreDiv.classList.add("hidden");
+  const choicesDiv = document.getElementById("choicelist")
   const choices = document.querySelectorAll(".bix");
-  const refreshButton = document.getElementById("refreshhbutton");
+  const levelUp = document.getElementById('buttonlevelup');
+  levelUp.classList.add("hidden");
+
   let compChoices = [];
   let compChoiceIndex = 0;
-
+  let score = compChoiceIndex
+  
   const nextRound = () => {
     let indx = Math.floor(Math.random() * 4);
     let value2 = choices[indx].getAttribute("id");
@@ -22,17 +25,21 @@ document.addEventListener("DOMContentLoaded", () => {
       box.addEventListener("click", () => {
         myChoice = box.id;
         console.log(myChoice);
+
         if (myChoice === compChoices[compChoiceIndex]) {
           compChoiceIndex++;
+          
           if (compChoiceIndex === compChoices.length) {
             console.log("win");
+            score++;  
 
             console.log(compChoiceIndex);
             setTimeout(() => {
               nextRound();
-            }, 1000);
+            }, 800);
           }
         } else {
+          console.log(score);
           showLose();
         }
       });
@@ -40,14 +47,14 @@ document.addEventListener("DOMContentLoaded", () => {
   };
   const showLose = () => {
     scoreDiv.classList.remove("hidden");
-    body.classList.add("brightness-50")
-     
+    body.classList.add("brightness-60");
+    scoreDiv.classList.add("brightness-200");
     
     scoreDiv.innerHTML = `
-    <pre class="bg-gray-900 text-white brightness-200 h-30 w-auto">Your Score : ${compChoiceIndex} </pre>
+    <pre class="bg-gray-900 text-white brightness-200 h-30 w-auto">Your Score : ${score} </pre>
 
     <p class="bg-amber-600 items-center flex justify-center "><button id = "refreshhbutton">Refresh</button></p>`;
-    console.log("YOu LOSE");
+    console.log("You LOSE");
   };
 
   const showCompChoices = () => {
@@ -56,21 +63,38 @@ document.addEventListener("DOMContentLoaded", () => {
         const button = document.getElementById(id);
         if (!button) return;
 
-        button.style.filter = "brightness(150%)";
+        button.style.filter = "brightness(200%)";
 
         setTimeout(() => {
           button.style.filter = "";
-        }, 300);
-      }, index * 600);
+        }, 700);
+      }, index * 800);
     });
   };
 
   startButton.addEventListener("click", () => {
     startButton.classList.add("hidden");
+    levelUp.classList.remove("hidden");
+    
+
     compChoices = [];
     nextRound();
     choiceChecker();
   });
+    levelUp.addEventListener("click", () => {
+    levelUp.classList.add("hidden");
+    choicesDiv.classList.add("animate-spin")
+    
+
+    
+    compChoices = [];
+    nextRound();
+    choiceChecker();
+  });
+  
+
+
+
   scoreDiv.addEventListener("click", (e) => {
     if (e.target.id === "refreshhbutton") location.reload();
   });
